a14c8bbec3a2049fc6039142049879772cbe4daa
KostyaAK <kostya_akulich_2000@mail.ru>
2020-11-25 21:16:26 +0300

    add clubs

---
+++ football_blog/Controllers/ClubsController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using football_blog.Models;
+using football_blog.Service;
+
+namespace football_blog.Controllers
+{
+    public class ClubsController : Controller
+    {
+        private readonly SiteContext _context;
+        private readonly IWebHostEnvironment _appEnvironment;
+        private readonly ImageService _imageService;
+        public ClubsController(SiteContext context, IWebHostEnvironment appEnvironment, ImageService imageService )
+        {
+                _context = context;
+                _appEnvironment = appEnvironment;
+                _imageService = imageService;
+        }
+
+       
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Clubs.ToListAsync());
+        }
+
+        
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dog = await _context.Clubs
+                .FirstOrDefaultAsync(m => m.ClubId == id);
+            if (dog == null)
+            {
+                return NotFound();
+            }
+
+            return View(dog);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+        
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ClubID,ClubName,ClubDescription")] Club club, IFormFile uploadedFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (uploadedFile != null && uploadedFile.ContentType.ToLower().Contains("image"))
+                {
+                    
+                    club.ClubImage =  await _imageService.SaveImageAsync(uploadedFile, 0);
+                    _context.SaveChanges();
+                }
+                else
+                {
+                    ModelState.AddModelError("Image", "Некорректный формат");
+                    return View(club);
+                }
+                _context.Add(club);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(club);
+        }
+        
+
+public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var club = await _context.Clubs.FindAsync(id);
+            if (club == null)
+            {
+                return NotFound();
+            }
+            return View(club);
+        }
+
+        // POST: Dogs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ClubId,ClubName,ClubDescription,ClubImage")] Club club, IFormFile uploadedFile)
+        {
+            if (id != club.ClubId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(club);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DogExists(club.ClubId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(club);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var club = await _context.Clubs
+                .FirstOrDefaultAsync(m => m.ClubId == id);
+            if (club == null)
+            {
+                return NotFound();
+            }
+
+            return View(club);
+        }
+
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var club = await _context.Clubs.FindAsync(id);
+            _context.Clubs.Remove(club);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DogExists(int id)
+        {
+            return _context.Clubs.Any(e => e.ClubId == id);
+        }
+    }
+}
