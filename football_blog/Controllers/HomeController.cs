bf16cfcced3a2c31662b2c9b03b8a1c2b6e19e2d
KostyaAK <kostya_akulich_2000@mail.ru>
2020-11-26 22:29:31 +0300

    design fix

--- football_blog/Controllers/HomeController.cs
+++ football_blog/Controllers/HomeController.cs
@@ -6,21 +6,24 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using football_blog.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace football_blog.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly SiteContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, SiteContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> IndexAsync()
         {
-            return View();
+            return View(await _context.Clubs.ToListAsync());
         }
 
         public IActionResult Privacy()
@@ -28,9 +31,9 @@ namespace football_blog.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public async Task<IActionResult> About()
         {
-            return View();
+            return View(await _context.Clubs.ToListAsync());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
